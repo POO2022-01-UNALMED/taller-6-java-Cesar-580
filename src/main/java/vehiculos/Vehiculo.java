e98a105f84051cbd1b407b5da8ea1425166be3cd
Cesar-580 <65823540+Cesar-580@users.noreply.github.com>
2022-05-10 16:47:54 -0500

    .

--- src/main/java/vehiculos/Vehiculo.java
+++ src/main/java/vehiculos/Vehiculo.java
@@ -11,7 +11,7 @@ public class Vehiculo {
 	private String traccion;
 	private Fabricante fabricante;
 	private static int CantidadVehiculos;
-	public static List<Vehiculo> vehiculos = new ArrayList<Vehiculo>();
+	public static ArrayList<Vehiculo> vehiculos = new ArrayList<Vehiculo>();
 	
 	public Vehiculo (String placa,int puertas,int velocidadMaxima,String nombre,int precio,int peso,String traccion,Fabricante fabricante) {
 		this.placa = placa;
